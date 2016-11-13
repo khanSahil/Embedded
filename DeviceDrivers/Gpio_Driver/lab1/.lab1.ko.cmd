29fb7ba4716f32a63113e70ac2e218bafee73809
khanSahil <sahilkhan.alld@gmail.com>
2016-11-13 15:49:07 -0700

    GPIO Driver for BeagleBone on Kernel Version = 3.8.13-bone81

---
+++ DeviceDrivers/Gpio_Driver/lab1/.lab1.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/debian/My_LDD_Programs/Gpio_Driver/lab1/lab1.ko := ld -EL -r  -T /usr/src/linux-headers-3.8.13-bone81/scripts/module-common.lds --build-id  -o /home/debian/My_LDD_Programs/Gpio_Driver/lab1/lab1.ko /home/debian/My_LDD_Programs/Gpio_Driver/lab1/lab1.o /home/debian/My_LDD_Programs/Gpio_Driver/lab1/lab1.mod.o
