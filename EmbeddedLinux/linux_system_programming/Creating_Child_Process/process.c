a5735c5786f38a4064bd773ac9afc3d77e6feb02
khanSahil <sahilkhan.alld@gmail.com>
2017-02-18 12:35:41 -0700

    Comments are added at the top of the file process.c

--- EmbeddedLinux/linux_system_programming/Creating_Child_Process/process.c
+++ EmbeddedLinux/linux_system_programming/Creating_Child_Process/process.c
@@ -1,9 +1,24 @@
+/*
+Author		: Sahil Khan
+Date		: 17th Feb 2017
+Description	: This lab demonstrates how a child process is created and modifies
+		  the data shared between parent and child process.Both local and global 
+		  data is modified and gets reflected when parent process is scheduled.
+		  Sleep() is called inside parent process just to make sure that child 
+		  runs first.
+		  In fork() system call , there is no gaurantee that child process runs
+		  first or parent process runs first.So to make sure this parent sleep 
+		  for 3 seconds and scheduler schedules child process and finally the 
+		  parent process.
+*/
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/stat.h>
-int globalData = 111;
 
+int globalData = 111;
 
 int main()
 {
