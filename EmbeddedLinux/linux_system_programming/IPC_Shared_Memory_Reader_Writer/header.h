6c6babfb2326f42d998f28c68c55b62459e7ddbd
khanSahil <sahilkhan.alld@gmail.com>
2017-03-16 21:14:45 -0600

    added function in the debug macros inside header.h file

--- EmbeddedLinux/linux_system_programming/IPC_Shared_Memory_Reader_Writer/header.h
+++ EmbeddedLinux/linux_system_programming/IPC_Shared_Memory_Reader_Writer/header.h
@@ -55,6 +55,9 @@ union semun {
 
 
 #define DEBUG 1
-#define pErr(...)	{printf("Trace_Error: "); printf(__VA_ARGS__);printf(" - %s:%d\n",__FILE__,__LINE__);exit(EXIT_FAILURE);}
-#define pDbg(...)	if (DEBUG) {printf("Trace_Debug: "); printf(__VA_ARGS__);printf(" - %s:%d\n",__FILE__,__LINE__);}
-#define pWar(...)	if (DEBUG) {printf("Trace_warning: "); printf(__VA_ARGS__);printf(" - %s:%d\n",__FILE__,__LINE__);}
+#define pErr(...)	{printf("Trace_Error: "); printf(__VA_ARGS__);printf(" - (%s:%s:%d)\n",__FILE__,__func__,__LINE__);exit(EXIT_FAILURE);}
+#define pDbg(...)	if (DEBUG) {printf("Trace_Debug: "); printf(__VA_ARGS__);printf(" - (%s:%s:%d)\n",__FILE__,__func__,__LINE__);}
+#define pWar(...)	if (DEBUG) {printf("Trace_warning: "); printf(__VA_ARGS__);printf(" - (%s:%s:%d)\n",__FILE__,__func__,__LINE__);}
+
+
+
